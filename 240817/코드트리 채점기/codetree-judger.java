6dc37e4118b7686827e54fee94e214586174dc47
sehunkim <sehun5515@naver.com>
2024-08-17 22:11:31 +0900

    Title: 코드트리 채점기, Time: 2242ms, Memory: 35MB, Status: Passed - CodeTree

--- 240817/코드트리 채점기/codetree-judger.java
+++ 240817/코드트리 채점기/codetree-judger.java
@@ -26,14 +26,12 @@ public class Main {
 			return result;
 		}
 
-		@Override
-		public String toString() {
-			return "Problem [time=" + time + ", priority=" + priority + ", url=" + url + "]";
-		}
 	}
 
 	// 대기중인 채점기의 id priority queue
 	private static PriorityQueue<Integer> judgeDevicePriorityQueue;
+	// 해당 도메인에 소속된 문제들을 저장하는 Map
+	private static Map<String, Set<Problem>> domainMap;
 	// 채점기의 상태(채점기 id, 채점기가 채점 중인 Problem)
 	private static Map<Integer, Problem> judgingDataMap;
 	// 가장 최근에 채점한 도메인의 데이터 맵
@@ -49,41 +47,42 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer cst;
 
 		int Q = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < Q; i++) {
-			st = new StringTokenizer(br.readLine());
-			int cmd = Integer.parseInt(st.nextToken());
+			cst = new StringTokenizer(br.readLine());
+			int cmd = Integer.parseInt(cst.nextToken());
 
 			if (cmd == 100) {
-				int N = Integer.parseInt(st.nextToken());
-				String url = st.nextToken();
+				int N = Integer.parseInt(cst.nextToken());
+				String url = cst.nextToken();
 
 				init(N, url);
 			} else if (cmd == 200) {
-				int t = Integer.parseInt(st.nextToken());
-				int p = Integer.parseInt(st.nextToken());
-				String url = st.nextToken();
+				int t = Integer.parseInt(cst.nextToken());
+				int p = Integer.parseInt(cst.nextToken());
+				String url = cst.nextToken();
 
 				requestJudge(t, p, url);
 			} else if (cmd == 300) {
-				int t = Integer.parseInt(st.nextToken());
+				int t = Integer.parseInt(cst.nextToken());
 
 				judge(t);
 			} else if (cmd == 400) {
-				int t = Integer.parseInt(st.nextToken());
-				int jid = Integer.parseInt(st.nextToken());
+				int t = Integer.parseInt(cst.nextToken());
+				int jid = Integer.parseInt(cst.nextToken());
 
 				exitJudge(t, jid);
 			} else if (cmd == 500) {
-				int t = Integer.parseInt(st.nextToken());
+				int t = Integer.parseInt(cst.nextToken());
 
 				sb.append(getJudgeTask(t)).append("\n");
 			}
 		}
 
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 
 	public static void init(int N, String url) {
@@ -93,6 +92,7 @@ public class Main {
 		waitingQueueProblemUrlSet = new HashSet<>();
 		waitingQueue = new PriorityQueue<>();
 		judgingDataMap = new HashMap<>();
+		domainMap = new HashMap<>();
 
 		for (int i = 1; i <= N; i++) {
 			judgeDevicePriorityQueue.add(i);
@@ -107,51 +107,72 @@ public class Main {
 		}
 
 		Problem problem = new Problem(t, p, url);
-		waitingQueue.add(problem);
+		String domain = getDomain(url);
+
+		if (!domainMap.containsKey(domain)) {
+			domainMap.put(domain, new HashSet<>());
+		}
+		domainMap.get(domain).add(problem);
 		waitingQueueProblemUrlSet.add(url);
 	}
 
+	/*
+	Map<String, Set<Problem>>의 자료구조를 하나 만든다.
+	String 은 도메인, TreeMap 에서 Integer 의 경우 해당 Problem 이 waiting queue 에 들어온 시간 t
+	채점 시 다음과 같은 로직을 구성한다.
+
+	1. Map 의 모든 key(도메인)을 가져온다.
+	2. 이 도메인 중 이미 채점이 진행중인 도메인들을 건너띈다.
+	3. 남은 도메인들에 대해 다음의 검증을 진행한다.
+		3-1. 해당 도메인에 대한 limit time 을 계산한다. 이때 limit time 은 위의 start + 3 * gap 이다.
+	 	3-2. 만약 현재 시간 t가 limit time 보다 작다면 해당 도메인은 건너띈다.
+	4. 남은 데이터들을 Priority Queue 에 밀어넣고 적합한 데이터를 가져온다.
+	5. profit
+	*/
 	public static void judge(int t) {
 		if (judgeDevicePriorityQueue.isEmpty()) {
 			return;
 		}
 
-		List<Problem> cannotJudgeProblemList = new ArrayList<>();
+		waitingQueue.clear();
 
-		while (!waitingQueue.isEmpty()) {
-			Problem problem = waitingQueue.poll();
-
-			st = new StringTokenizer(problem.url, "/");
-			String domain = st.nextToken();
-
+		for (String domain : domainMap.keySet()) {
+			// 도메인 중 이미 채점이 진행중인 도메인들을 건너띈다.
 			if (judgingProblemDomainSet.contains(domain)) {
-				cannotJudgeProblemList.add(problem);
 				continue;
 			}
 
 			if (problemHistoryMap.containsKey(domain)) {
-				Problem judgedProblem = problemHistoryMap.get(domain);
+				Problem judgedHistoryProblem = problemHistoryMap.get(domain);
 
-				int gap = judgedProblem.endTime - judgedProblem.startTime;
-				int limitTime = judgedProblem.startTime + 3 * gap;
+				// 해당 도메인에 대한 limit time 을 계산한다. 이때 limit time 은 위의 start + 3 * gap 이다.
+				int gap = judgedHistoryProblem.endTime - judgedHistoryProblem.startTime;
+				int limitTime = judgedHistoryProblem.startTime + 3 * gap;
 
-				if (limitTime > t) {
-					cannotJudgeProblemList.add(problem);
+				// 만약 현재 시간 t가 limit time 보다 작다면 해당 도메인은 건너띈다.
+				if (t < limitTime) {
 					continue;
 				}
 			}
 
-			int judgerId = judgeDevicePriorityQueue.poll();
-
-			problem.startTime = t;
-			judgingProblemDomainSet.add(domain);
-			judgingDataMap.put(judgerId, problem);
-			waitingQueueProblemUrlSet.remove(problem.url);
-
-			break;
+			// 4. 남은 데이터들을 Priority Queue 에 밀어넣는다.
+			waitingQueue.addAll(domainMap.get(domain));
 		}
 
-		waitingQueue.addAll(cannotJudgeProblemList);
+		if (waitingQueue.isEmpty()) {
+			return;
+		}
+
+		Problem problem = waitingQueue.poll();
+		int judgerId = judgeDevicePriorityQueue.poll();
+
+		problem.startTime = t;
+		String domain = getDomain(problem.url);
+		judgingProblemDomainSet.add(domain);
+		judgingDataMap.put(judgerId, problem);
+
+		domainMap.get(domain).remove(problem);
+		waitingQueueProblemUrlSet.remove(problem.url);
 	}
 
 	public static void exitJudge(int t, int jid) {
@@ -175,7 +196,19 @@ public class Main {
 	}
 
 	public static int getJudgeTask(int t) {
-		return waitingQueue.size();
+		int res = 0;
+
+		for (String domain : domainMap.keySet()) {
+			res += domainMap.get(domain).size();
+		}
+
+		return res;
+	}
+
+	private static String getDomain(String url) {
+		st = new StringTokenizer(url, "/");
+
+		return st.nextToken();
 	}
 
 }
