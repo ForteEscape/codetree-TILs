e76af66937c98b683d81596aaefe453fa4b8c97a
sehunkim <sehun5515@naver.com>
2024-09-06 17:34:27 +0900

    Title: 싸움땅, Time: 265ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240906/싸움땅/battle-ground.java
+++ 240906/싸움땅/battle-ground.java
@@ -80,6 +80,7 @@ public class Main {
 			int ny = players[i].y + dy[players[i].dir];
 			int nx = players[i].x + dx[players[i].dir];
 
+			// 격자를 벗어나는 경우에는 정반대 방향으로 방향을 바꾸어서 1만큼 이동합니다.
 			if (ny < 0 || ny >= N || nx < 0 || nx >= N) {
 				players[i].dir = (players[i].dir + 2) % 4;
 				ny = players[i].y + dy[players[i].dir];
@@ -92,12 +93,19 @@ public class Main {
 			for (int j = 0; j < players.length; j++) {
 				if (i == j) continue;
 
+				// 만약 이동한 방향에 플레이어가 있는 경우에는 두 플레이어가 싸우게 됩니다.
 				if (players[i].y == players[j].y && players[i].x == players[j].x) {
 					flag = true;
 					battle(i, j);
 				}
 			}
 
+			/*
+			만약 이동한 방향에 플레이어가 없다면 해당 칸에 총이 있는지 확인합니다.
+			총이 있는 경우, 해당 플레이어는 총을 획득합니다.
+			플레이어가 이미 총을 가지고 있는 경우에는 놓여있는 총들과 플레이어가 가지고 있는 총 가운데
+			공격력이 더 쎈 총을 획득하고, 나머지 총들은 해당 격자에 둡니다.
+			 */
 			if (!flag) {
 				if (!board[players[i].y][players[i].x].isEmpty()) {
 					if (players[i].gunStatus == 0) {
@@ -113,6 +121,11 @@ public class Main {
 		}
 	}
 
+	/*
+	당 플레이어의 초기 능력치와 가지고 있는 총의 공격력의 합을 비교하여 더 큰 플레이어가 이기게 됩니다.
+	만일 이 수치가 같은 경우에는 플레이어의 초기 능력치가 높은 플레이어가 승리하게 됩니다.
+	이긴 플레이어는 각 플레이어의 초기 능력치와 가지고 있는 총의 공격력의 합의 차이만큼을 포인트로 획득하게 됩니다.
+	 */
 	private static void battle(int playerId, int otherPlayerId) {
 		int currentPlayerStatus = players[playerId].status + players[playerId].gunStatus;
 		int otherPlayerStatus = players[otherPlayerId].status + players[otherPlayerId].gunStatus;
@@ -139,13 +152,22 @@ public class Main {
 	private static void runOut(int playerId) {
 		Player player = players[playerId];
 
+		/*
+		진 플레이어는 본인이 가지고 있는 총을 해당 격자에 내려놓고,
+		해당 플레이어가 원래 가지고 있던 방향대로 한 칸 이동합니다.
+		 */
 		board[player.y][player.x].offer(player.gunStatus);
 		player.gunStatus = 0;
 
+		/*
+		만약 이동하려는 칸에 다른 플레이어가 있거나 격자 범위 밖인 경우에는
+		오른쪽으로 90도씩 회전하여 빈 칸이 보이는 순간 이동합니다.
+		 */
 		while(true) {
 			boolean flag = false;
 			int ny = player.y + dy[player.dir];
 			int nx = player.x + dx[player.dir];
+			System.out.println(player.dir);
 
 			if (ny < 0 || ny >= N || nx < 0 || nx >= N) {
 				player.dir = (player.dir + 1) % 4;
@@ -165,8 +187,14 @@ public class Main {
 				player.x = nx;
 				break;
 			}
+
+			player.dir = (player.dir + 1) % 4;
 		}
 
+		/*
+		만약 해당 칸에 총이 있다면, 해당 플레이어는 가장 공격력이 높은 총을 획득하고
+		나머지 총들은 해당 격자에 내려 놓습니다.
+		 */
 		if (!board[player.y][player.x].isEmpty()) {
 			player.gunStatus += board[player.y][player.x].poll();
 		}
