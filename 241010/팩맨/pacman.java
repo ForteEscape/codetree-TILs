1fec347935585cb9bc0aa2fa4d2ef478d52db9e2
sehunkim <sehun5515@naver.com>
2024-10-10 13:05:19 +0900

    Title: 팩맨, Time: 3102ms, Memory: 224MB, Status: Time Limit Exceed - CodeTree

--- 241010/팩맨/pacman.java
+++ 241010/팩맨/pacman.java
@@ -12,14 +12,12 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static class Monster {
-		int idx;
 		int y;
 		int x;
 		int dir;
 		int destroyTurn;
 
-		public Monster(int idx, int y, int x, int dir) {
-			this.idx = idx;
+		public Monster(int y, int x, int dir) {
 			this.y = y;
 			this.x = x;
 			this.dir = dir;
@@ -42,14 +40,15 @@ public class Main {
 	private static int[] dx = {0, -1, -1, -1, 0, 1, 1, 1};
 	private static int[] packmanDy = {-1, 0, 1, 0};
 	private static int[] packmanDx = {0, -1, 0, 1};
-	private static List<List<Monster>> map;
 	private static Packman packman;
-	private static Map<Integer, Monster> monsterMap;
 	private static int idx;
-	private static Map<Integer, Monster> eggMonsterMap;
-	private static Map<Integer, Monster> corpsMonsterMap;
 	private static int maxCnt;
 	private static List<int[]> maxMovement;
+	private static Set<Monster>[][] map;
+	private static Set<Monster>[][] corps;
+	private static Map<Integer, List<Monster>> corpsMap;
+	private static Set<Monster> aliveMonster;
+	private static Set<Monster> eggMonster;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -63,7 +62,18 @@ public class Main {
 		int initX = Integer.parseInt(st.nextToken());
 		packman = new Packman(initY, initX);
 
-		monsterMap = new HashMap<>();
+		map = new Set[5][5];
+		corps = new Set[5][5];
+		corpsMap = new HashMap<>();
+		eggMonster = new HashSet<>();
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				corps[i][j] = new HashSet<>();
+				map[i][j] = new HashSet<>();
+			}
+		}
+
+		aliveMonster = new HashSet<>();
 		for (idx = 0; idx < N; idx++) {
 			st = new StringTokenizer(br.readLine());
 
@@ -71,14 +81,13 @@ public class Main {
 			int x = Integer.parseInt(st.nextToken());
 			int dir = Integer.parseInt(st.nextToken());
 
-			monsterMap.put(idx, new Monster(idx, y, x, dir - 1));
+			Monster monster = new Monster(y, x, dir - 1);
+			map[y][x].add(monster);
+			aliveMonster.add(monster);
 		}
-
-		corpsMonsterMap = new HashMap<>();
 
 		int turn = 1;
 		while (turn <= T) {
-			eggMonsterMap = new HashMap<>();
 			monsterClone();
 			monsterMove();
 			packmanMove(turn);
@@ -88,20 +97,17 @@ public class Main {
 			turn++;
 		}
 
-		System.out.println(monsterMap.size());
+		System.out.println(aliveMonster.size());
 	}
 
 	public static void monsterClone() {
-		for (int element : monsterMap.keySet()) {
-			Monster monster = monsterMap.get(element);
-			eggMonsterMap.put(idx, new Monster(idx++, monster.y, monster.x, monster.dir));
+		for (Monster monster : aliveMonster) {
+			eggMonster.add(new Monster(monster.y, monster.x, monster.dir));
 		}
 	}
 
 	public static void monsterMove() {
-		for (int element : monsterMap.keySet()) {
-			Monster monster = monsterMap.get(element);
-
+		for (Monster monster : aliveMonster) {
 			int dir = findMoveableDirection(monster);
 
 			if (dir != -1) {
@@ -110,45 +116,22 @@ public class Main {
 				monster.x = monster.x + dx[monster.dir];
 			}
 		}
-
-		for (int element : monsterMap.keySet()) {
-			Monster monster = monsterMap.get(element);
-			//System.out.println("move info : " + element + " " + monster.y + " " + monster.x + " " + monster.dir);
-		}
-
-		map = new ArrayList<>();
-		for (int i = 0; i <= 4; i++) {
-			for (int j = 0; j <= 4; j++) {
-				map.add(new ArrayList<>());
-			}
-		}
-
-		for (int element : monsterMap.keySet()) {
-			Monster monster = monsterMap.get(element);
-			map.get(monster.y * 5 + monster.x).add(monster);
-		}
 	}
 
 	private static int findMoveableDirection(Monster monster) {
 		int dir = monster.dir;
 
 		for (int i = 0; i < 8; i++) {
-			boolean isCorpsExists = false;
 			int ny = monster.y + dy[dir];
 			int nx = monster.x + dx[dir];
 
-			for (int element : corpsMonsterMap.keySet()) {
-				Monster corps = corpsMonsterMap.get(element);
-				if (corps.y == ny && corps.x == nx) {
-					isCorpsExists = true;
-					break;
-				}
-			}
-
-			if (isCorpsExists || isUnreachable(ny, nx) || (packman.y == ny && packman.x == nx)) {
+			if (isUnreachable(ny, nx) || !corps[ny][nx].isEmpty() || (packman.y == ny && packman.x == nx)) {
 				dir = (dir + 1) % 8;
 				continue;
 			}
+
+			map[monster.y][monster.x].remove(monster);
+			map[ny][nx].add(monster);
 
 			return dir;
 		}
@@ -161,16 +144,17 @@ public class Main {
 		maxMovement = new ArrayList<>();
 
 		getMoveablePosition(0, new int[] {packman.y, packman.x}, new ArrayList<>());
+		if (!corpsMap.containsKey(currentTurn + 2)) {
+			corpsMap.put(currentTurn + 2, new ArrayList<>());
+		}
 
 		for (int[] row : maxMovement) {
-			//System.out.println("move row : " + row[0] + " " + row[1]);
-			for (Monster monster : map.get(row[0] * 5 + row[1])) {
-				monster.destroyTurn = currentTurn + 2;
-
-				corpsMonsterMap.put(monster.idx, monster);
-				monsterMap.remove(monster.idx);
+			for (Monster monster : map[row[0]][row[1]]) {
+				corps[monster.y][monster.x].add(monster);
+				corpsMap.get(currentTurn + 2).add(monster);
+				aliveMonster.remove(monster);
 			}
-
+			map[row[0]][row[1]] = new HashSet<>();
 			packman.y = row[0];
 			packman.x = row[1];
 		}
@@ -182,7 +166,7 @@ public class Main {
 
 			Set<Integer> set = new HashSet<>();
 			for (int[] row : positionData) {
-				cnt += set.contains(row[0] * 5 + row[1]) ? 0 : map.get(row[0] * 5 + row[1]).size();
+				cnt += set.contains(row[0] * 5 + row[1]) ? 0 : map[row[0]][row[1]].size();
 				set.add(row[0] * 5 + row[1]);
 			}
 
@@ -212,24 +196,23 @@ public class Main {
 	}
 
 	public static void monsterCorpsDestroy(int currentTurn) {
-		List<Integer> destroyKeyList = new ArrayList<>();
-
-		for (int element : corpsMonsterMap.keySet()) {
-			if (corpsMonsterMap.get(element).destroyTurn == currentTurn) {
-				destroyKeyList.add(element);
-			}
+		if (!corpsMap.containsKey(currentTurn)) {
+			return;
 		}
 
-		for (int element : destroyKeyList) {
-			corpsMonsterMap.remove(element);
+		List<Monster> corpsList = corpsMap.get(currentTurn);
+		for (Monster monster : corpsList) {
+			corps[monster.y][monster.x].remove(monster);
 		}
+		corpsMap.remove(currentTurn);
 	}
 
 	public static void monsterCloneFinish() {
-		for (int element : eggMonsterMap.keySet()) {
-			Monster monster = eggMonsterMap.get(element);
-			monsterMap.put(monster.idx, monster);
+		for (Monster monster : eggMonster) {
+			map[monster.y][monster.x].add(monster);
+			aliveMonster.add(monster);
 		}
+		eggMonster = new HashSet<>();
 	}
 
 	private static boolean isUnreachable(int y, int x) {
